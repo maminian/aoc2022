373bef5ac8c9dd46091b6483c48dbd50ded23828
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2022-12-07 22:40:43 -0800

    fooling aroung with vis

--- dec08/dec08.py
+++ dec08/dec08.py
@@ -1,5 +1,8 @@
 import csv
 import numpy as np
+from matplotlib import pyplot
+
+pyplot.style.use('dark_background')
 
 with open('input', 'r') as f:
     huh = csv.reader(f)
@@ -81,4 +84,35 @@ for i in range(1,data.shape[0]-1):
 best = np.argmax(all_scores)
 best = np.unravel_index(best, all_scores.shape)
 print('best spot: ', best, '; best score: ', all_scores[best])
+
+#
+# fun vis time!
+#
+from matplotlib import pyplot, colors, cm
+import colorcet
+
+X = np.tile(np.arange(0,data.shape[1])[::-1], (data.shape[0],1))
+S = X + X.T
+
+shademap = S*(scanx(data) + scany(data))
+interior = shademap[1:-1,1:-1]
+
+th = 0.1
+for i in range(3):
+    shademap[1:-1,1:-1] = th*shademap[1:-1,1:-1] + (1-th)/4*(shademap[:-2,1:-1] + shademap[2:,1:-1] + shademap[1:-1,:-2] + shademap[1:-1,2:])
+
+forestgreen = [0.133,0.545,0.133]
+mycm = colors.LinearSegmentedColormap.from_list('kg', [[0,0,0], forestgreen])
+
+fig,ax = pyplot.subplots(constrained_layout=True)
+ax.pcolor(data[1:,1:], cmap=mycm, edgecolor=None)
+ax.pcolor(shademap[1:,1:], cmap=pyplot.cm.magma, alpha=0.7, edgecolor=None)
+ax.set_aspect('equal')
+ax.set_xticks([])
+ax.set_yticks([])
+for v in ax.spines.values():
+    v.set_visible(False)
+
+fig.savefig('elf_forest_sunset.png', bbox_inches='tight')
+fig.show()
 
