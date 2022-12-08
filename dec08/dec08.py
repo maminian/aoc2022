a44b95c1af172a10718f75764871e83094c54510
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2022-12-07 21:42:27 -0800

    part 2

--- dec08/dec08.py
+++ dec08/dec08.py
@@ -37,7 +37,48 @@ def sweep(arr):
     return left + right + top + bottom
 #
 
+def eval_view(arr,i,j):
+    # west
+    views = np.zeros(4, dtype=int)
+    for p in range(i-1,-1,-1):
+        views[0] += 1
+        if arr[p,j] >= arr[i,j]:
+#            print(i,j,p,'l')
+            break
+    # east
+    for p in range(i+1,arr.shape[0],1):
+        views[1] += 1
+        if arr[p,j] >= arr[i,j]:
+#            print(i,j,p,'r')
+            break
+    # north
+    for q in range(j-1,-1,-1):
+        views[2] += 1
+        if arr[i,q] >= arr[i,j]:
+#            print(i,j,q,'n')
+            break
+    # south
+    for q in range(j+1,arr.shape[1],1):
+        views[3] += 1
+        if arr[i,q] >= arr[i,j]:
+#            print(i,j,q,'s')
+            break
+    return views
+
+# part 1
 vis = sweep(data)
-#print(data)
-#print(vis)
 print("visible trees: ",vis.sum())
+
+
+# part 2: maximum viewing distance?
+all_scores = np.zeros(data.shape, dtype=int)
+for i in range(1,data.shape[0]-1):
+    for j in range(1,data.shape[1]-1):
+        views = eval_view(data,i,j)
+#        print(i,j, views)
+        all_scores[i,j] = views.prod()
+
+best = np.argmax(all_scores)
+best = np.unravel_index(best, all_scores.shape)
+print('best spot: ', best, '; best score: ', all_scores[best])
+
